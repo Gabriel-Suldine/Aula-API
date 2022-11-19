336f1e7fa4bce01d6adde83f8eff9903a4272762
Gabriel Suldine <82013387+Gabriel-Suldine@users.noreply.github.com>
2022-11-18 22:41:53 -0400

    ✨incrementando loader

--- api/main.js
+++ api/main.js
@@ -1,11 +1,18 @@
+import '@picocss/pico';
 const formConsultarCep = document.querySelector('#consultarCep');
-const inputCep = formConsultarCep.cep
+const inputCep = formConsultarCep.cep;
 const divDados = document.querySelector('#dados')
+const btnConsultarCep = document.querySelector('#btnConsultarCep')
+/* const loader = `<a href="#" aria-busy="true">Consultando CEP, aguarde um instante</a>`;
+  alt + shift+a comenta tudo selecionado*/
+
 
 
 formConsultarCep.addEventListener('submit', event => {
   event.preventDefault()  //anula o comportamento padrão de envio do form  
+  ativaLoader(true)
   consultarCep(inputCep.value)
+
 })
 
 
@@ -13,8 +20,20 @@ async function consultarCep(cep) {
   let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
   let dadosCep = await response.json()
   divDados.innerHTML = `
-<p>Endereço: ${dadosCep.logradouro}</p>
-<p>Localidade: ${dadosCep.localidade} </p>
-<p>UF:${dadosCep.uf} </p>
-`}
+    <p>Endereço: ${dadosCep.logradouro}</p>
+    <p>Localidade: ${dadosCep.localidade} </p>
+    <p>UF:${dadosCep.uf} </p>
+    <p>DDD:${dadosCep.ddd} </p>
+`
+ativaLoader(false)
+}
 
+function ativaLoader(ativo) {
+  if (ativo) {
+    btnConsultarCep.setAttribute('aria-busy', 'true')
+    btnConsultarCep.textContent = 'Consultando Cep...'
+  }else{
+    btnConsultarCep.removeAttribute('aria-busy')
+    btnConsultarCep.textContent = 'Consultar'
+  }
+}
